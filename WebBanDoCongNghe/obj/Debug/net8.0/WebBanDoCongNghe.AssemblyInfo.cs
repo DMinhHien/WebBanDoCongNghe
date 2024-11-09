1efdbda66496152a3c00d0dfe1b59cbd76b6f3c6
duongtx <duongtx.t2.1922@gmail.com>
2024-11-09 10:46:01 +0700

    commit

--- WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
+++ WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1be136832a3a26fe2e9e3c395ddd687ea84aff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f110935ca1b38e391cd5910eb106001135d1c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
