e2382d8ad09ed627ecf97e40640a8639f1ab69aa
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2024-12-13 15:14:15 +0700

    addrole

--- WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
+++ WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27171a51859e07067bd80342d9121dd6bc62ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ce786c69c095044181e01039e732bf398669c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
