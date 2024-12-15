5f857c9bb5ea5d316f92ed8aacc419fbef273cb0
MinhHien <150352316+DMinhHien@users.noreply.github.com>
2024-12-15 16:42:50 +0700

    Datetime now

--- WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
+++ WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bcdf7e1862a2ab4b1185d7437245ce00c20c48e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a857bd6f0b84b247d1e0e4ee1fd05adc8f67a64b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
