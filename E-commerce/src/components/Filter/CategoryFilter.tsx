8c6921e2e45b926a14afd4a3ffbdc990ed9339e5
duongtx <duongtx.t2.1922@gmail.com>
2024-12-16 12:13:13 +0700

    shop details

--- E-commerce/src/components/Filter/CategoryFilter.tsx
+++ E-commerce/src/components/Filter/CategoryFilter.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import {
     Accordion,
     AccordionSummary,
@@ -11,12 +12,26 @@ import {
   } from "@mui/material";
   import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
   import useProductStore from "../../zustand/useProductStore"
-  import { categories } from "../../data/products";
-
+  import { Category } from "../../data/category";
   const CategoryFilter = () => {
     const { filters, setCategoryFilter } = useProductStore();
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
     const [selectedCategory, setSelectedCategory] = useState(filters.category);
-  
+    useEffect(() => {
+          fetchCategories();
+        }, []);
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get<Category[]>('https://localhost:7183/Categories/getListUse');
+        setCategories(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError('Không thể lấy dữ liệu category');
+        setLoading(false);
+      }
+    };
     const handleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       const category = event.target.value;
       setSelectedCategory(category);
@@ -41,13 +56,13 @@ import {
           >
             <Grid container>
               {categories.map((category) => (
-                <Grid item xs={6} key={category}>
+                <Grid item xs={6} key={category.id}>
                   {" "}
                   {/* Mỗi phần tử chiếm 50% chiều rộng */}
                   <FormControlLabel
-                    value={category}
+                    value={category.name}
                     control={<Radio />}
-                    label={category}
+                    label={category.name}
                   />
                 </Grid>
               ))}
--- WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
+++ WebBanDoCongNghe/obj/Debug/net8.0/WebBanDoCongNghe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3417b3abb45f6db12218d883a87ac796a5f82fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5748d39967ffd817c96abbb6acc4f62ffd1d82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanDoCongNghe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
